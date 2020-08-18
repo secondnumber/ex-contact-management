623de93baf694cb6e08e2b0137dd6eb867e2bc8e
Yulya Vorontsova <yulyavorontsova@Yulyas-MacBook-Air.local>
2020-08-18 10:59:43 +0300

    init: initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import './App.less';
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+          type="primary"
+          icon={<SearchOutlined/>}
+          data-testid="add-contact-button"
+      >
+          Button
+      </Button>
     </div>
-  );
-}
+);
 
 export default App;
